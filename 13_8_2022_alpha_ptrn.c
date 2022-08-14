67aa516ed72697d3d16852b53b92c0a5d1cc1710
kssvinay <110870903+kssvinay@users.noreply.github.com>
2022-08-14 07:11:01 +0530

    Create 13_8_2022_alpha_ptrn.c

---
+++ 13_8_2022_alpha_ptrn.c
@@ -0,0 +1,12 @@
+/*1:Write a C  program to read “n” value and print following pattern:
+Note: use “\t” for spaces.
+Test Case1:
+Input: 
+Enter n value:5
+Output:                                     
+Aa
+Bb      Cc
+Dd      Ee      Ff
+Gg      Hh      Ii      Jj
+Kk      Ll      Mm      Nn      Oo
+*/
