6bae60a643e5db965b9207f82b5b760ba7578650
kssvinay <110870903+kssvinay@users.noreply.github.com>
2022-08-14 07:13:12 +0530

    Update 13_8_2022_alpha_ptrn.c

--- 13_8_2022_alpha_ptrn.c
+++ 13_8_2022_alpha_ptrn.c
@@ -1,6 +1,4 @@
 /*1:Write a C  program to read “n” value and print following pattern:
-Note: use “\t” for spaces.
-Test Case1:
 Input: 
 Enter n value:5
 Output:                                     
@@ -10,3 +8,18 @@ Dd      Ee      Ff
 Gg      Hh      Ii      Jj
 Kk      Ll      Mm      Nn      Oo
 */
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    char c = 'A';
+    printf("enter n value");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%3c%c",c,c+32);
+            c++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
