023d327bb1e0d9923d0eec0d6c874540477a8c98
kssvinay <110870903+kssvinay@users.noreply.github.com>
2022-08-26 09:09:16 +0530

    Create 25_8_2022_cnvrt_alt_2_upr.c

---
+++ 25_8_2022_cnvrt_alt_2_upr.c
@@ -0,0 +1,26 @@
+/*
+ Program to read a  string and convert alternative character into upper case  
+Input: Enter string: aditya
+Output: aDiTyA
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    char str[30];
+    int i;
+    printf("Enter String : ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if( (i % 2) == 0)
+            str[i] = tolower(str[i]);
+        else
+            str[i] = toupper(str[i]);
+    }
+    printf("\n%s", str);
+    return 0;
+}
