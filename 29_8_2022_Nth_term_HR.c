376f426faa53d16493fe348ef5ee1f1b7d08f63d
kssvinay <110870903+kssvinay@users.noreply.github.com>
2022-08-30 09:53:33 +0530

    Update 29_8_2022_Nth_term_HR.c

--- 29_8_2022_Nth_term_HR.c
+++ 29_8_2022_Nth_term_HR.c
@@ -8,19 +8,19 @@ https://www.hackerrank.com/challenges/recursion-in-c/problem?isFullScreen=true
 #include <stdlib.h>
 //Complete the following function.
 
-int sum(int n){
-    if(n==1){return 1;}
-    else if(n==2){return 2;}
-    else if(n==3){return 3;}
+int sum(int n,int a,int b, int c){
+    if(n==1){return a;}
+    else if(n==2){return b;}
+    else if(n==3){return c;}
     else{
-        return sum(n-1)+sum(n-2)+sum(n-3);
+        return sum(n-1,a,b,c)+sum(n-2,a,b,c)+sum(n-3,a,b,c);
     }
 }
 int main() {
     int n, a, b, c;
   
     scanf("%d %d %d %d", &n, &a, &b, &c);
-    int ans = sum(n);
+    int ans = sum(n,a,b,c);
  
     printf("%d", ans); 
     return 0;
